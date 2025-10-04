ea69b653599523075b5ce293c568b158a73f0a5d
Tyler Pollard <tylerpollard410@gmail.com>
2025-10-03 21:13:11 -0400

    added ATS resume

--- assets/Resume/render_resume.R
+++ assets/Resume/render_resume.R
@@ -1,10 +1,35 @@
-## Script to render Rmarkdown to pdf
+## Script to render Quarto to pdf
 # Author: Tyler Pollard
 # Date: 22Aug2024
 
-library(rmarkdown)
+# library(rmarkdown)
 
-render(input = "assets/Resume/Resume.Rmd", 
-       output_format = "pdf_document", 
-       output_dir = "assets/download",
-       output_file = "resume.pdf")
+# render(
+#        input = "assets/Resume/Resume.Rmd",
+#        output_format = "pdf_document",
+#        output_dir = "assets/download",
+#        output_file = "resume.pdf"
+# )
+
+library(quarto)
+
+quarto_render(
+       input = "assets/Resume/Resume.qmd",
+       output_format = "pdf",
+       output_file = "resume.pdf",
+       quarto_args = c("--output-dir", "../download")
+)
+
+quarto_render(
+       input = "assets/Resume/ResumeATS.qmd",
+       output_format = "pdf",
+       output_file = "resumeATS.pdf",
+       quarto_args = c("--output-dir", "../download")
+)
+
+quarto_render(
+       input = "assets/Resume/ResumeAwsCV.qmd",
+       output_format = "awesomecv-typst",
+       output_file = "resumeCV.pdf",
+       quarto_args = c("--output-dir", "../download")
+)
